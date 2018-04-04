a18dbffdcc41b241eb88d386ba467f7b5c72c5d1
sifenkesi9 <282570694@qq.com>
2018-04-04 08:17:04 +0800

    添加博客类型表

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from blog.models import Blog
+from blog.models import Blog, BlogType
+
+admin.site.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
 
 admin.site.register(Blog)
 
--- blog/models.py
+++ blog/models.py
@@ -6,16 +6,24 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type_name
+
 class Blog(models.Model):
     class Meta:
         verbose_name = _('博客')
         verbose_name_plural = _('博客')
 
     title = models.CharField(max_length=100, unique=True, verbose_name=_('标题'), help_text='博客的标题')
-    author = models.ForeignKey(User, verbose_name=_('作者'), on_delete=models.CASCADE)
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING, null=True)
+    author = models.ForeignKey(User, verbose_name=_('作者'), on_delete=models.DO_NOTHING)
     slug = models.SlugField(max_length=50, unique=True, verbose_name=_('URL'))
     body = RichTextUploadingField(verbose_name=_('正文'))
     posted = models.DateField(db_index=True, auto_now_add=True)
+
 
     def __str__(self):
         return '%s' % (self.title)
