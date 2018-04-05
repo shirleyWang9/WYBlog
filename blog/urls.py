28936aa0793d206a2a0895918d6078af1874779c
sifenkesi9 <282570694@qq.com>
2018-04-05 09:57:00 +0800

    博客分类

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from blog.views import blog_detail
+from blog.views import blog_detail,blog_with_type
 
 
 urlpatterns = [
-    url(r'^(?P<slug>[^\.]+).html', blog_detail, name='blog_view'),
+
+    url(r'^(?P<slug>(?!-)(?!.*?-$)[0-9a-zA-Z]+(-)+[^\.]+).html', blog_detail, name='blog_view'),
+    url(r'^type/(?P<blog_type_pk>[0-9]+).html', blog_with_type, name='blog_with_type'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 
-from blog.models import Blog
+from blog.models import Blog,BlogType
 
 from django.contrib.auth.models import User
 
@@ -15,6 +15,15 @@ def blog_list(request):
 def blog_detail(request,slug):
     return render_to_response('blog/detail.html',{
         'blog': get_object_or_404(Blog, slug=slug)
+    })
+
+def blog_with_type(request,blog_type_pk):
+
+    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+    return render_to_response('blog/blog_with_type.html',{
+        'blogs': Blog.objects.filter(blog_type=blog_type),
+        'blog_type': blog_type
+
     })
 
 # def index(request):
